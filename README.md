882d945e099e727aecea17f4a5bf161c90c6d2d6
SHIBA07-Fullstack <175839673+SHIBA07-Fullstack@users.noreply.github.com>
2024-10-20 23:02:53 +0530

    Update script for DOM manipulation, update readme and fix css

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rock-paper-scissors-game
+
+Images provided by :
+
+>Freepik : https://www.flaticon.com/free-icon/fist_9496176?related_id=9496176
+
+>Cap Cool : https://www.flaticon.com/free-icon/scissors_9534501?term=rock&page=1&position=8&origin=tag&related_id=9534501
+
+>iconading : https://www.flaticon.com/free-icon/hand-paper_12355903?term=rock&page=1&position=10&origin=tag&related_id=12355903
--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 // Script for Rock Paper Scissor game
 
-
-
-function startGame(){
-
-console.clear();
 let text = document.querySelector("#demo");
 const win = "You won against computer!";
 const draw = "It's draw!";
@@ -12,10 +7,19 @@ const lose = "Computer won!";
 let i;
 let humanScore = 0;
 let computerScore = 0;
-document.getElementById("start").innerHTML = "Restart Game!";
-text.textContent = text.textContent.slice(text.textContent.length);
-
-
+let humanSelection = "";
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissor = document.querySelector(".scissor"); 
+const image = document.createElement("img");
+const image2 = document.createElement("img");
+const pcChoice = document.querySelector(".computerChoice");
+const playerChoice = document.querySelector(".playerChoice");
+const announcementBox = document.querySelector(".announcementBox");
+const playerScoreBox = document.querySelector(".playerNum");
+const computerScoreBox = document.querySelector(".computerNum");
+computerScoreBox.textContent = computerScore;
+playerScoreBox.textContent = humanScore;
 
 // Getting random computer choice
 
@@ -25,115 +29,133 @@ function getComputerChoice(){
     randomNo = Math.ceil(Math.random() * 3);
     if (randomNo == 1){
         CompChoice = 'rock';
+        image.src = "./imgs/fist-rock.png";
+        image.setAttribute("style", "height: 150px; width: 150px;");
+        pcChoice.appendChild(image);
     }
     else if (randomNo == 2){
         CompChoice = 'paper';
+        image.src = "./imgs/hand-paper.png";
+        image.setAttribute("style", "height: 150px; width: 150px;");
+        pcChoice.appendChild(image);
     }
     else{
         CompChoice = 'scissor';
+        image.src = "./imgs/scissors.png";
+        image.setAttribute("style", "height: 150px; width: 150px;");
+        pcChoice.appendChild(image);
     }
     return CompChoice;
 }
 
+let computerSelection;
+
 // Getting human choice
 
-function getHumanChoice() {
+
     
-    let input = prompt("Enter your choice:");
-    
-    if(!input){
-        return alert("Canceled");
-    } 
-    else if (input.toLowerCase() === "rock"||input.toLowerCase() === "paper"||input.toLowerCase() === "scissor"){
-        return input = input.toLowerCase();  
-    }
-    else{ 
-        alert("Enter valid choice:");
-        return getHumanChoice();  
-    }         
-}
+rock.addEventListener("click", () =>{
+    image2.src = "./imgs/fist-rock.png";
+    image2.setAttribute("style", "height: 150px; width: 150px;");
+    playerChoice.appendChild(image2);
+    humanSelection = "rock";
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+});
+   
+paper.addEventListener("click", () =>{
+    image2.src = "./imgs/hand-paper.png";
+    image2.setAttribute("style", "height: 150px; width: 150px;");
+    playerChoice.appendChild(image2);
+    humanSelection = "paper";
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+});
+
+scissor.addEventListener("click", () =>{
+    image2.src = "./imgs/scissors.png";
+    image2.setAttribute("style", "height: 150px; width: 150px;");
+    playerChoice.appendChild(image2);
+    humanSelection = "scissor";
+    computerSelection = getComputerChoice();
+    console.log(computerSelection);
+    console.log(humanSelection);
+    playRound(humanSelection, computerSelection);
+})
+   
 
 // Playing a full game
-
-
-
-
-for(i=1;i<=5;i++){ 
     
 
-    setTimeout(function playGame(){
-        
-        
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        console.log(computerSelection);
-        console.log(humanSelection);
-        
         // Single game round function 
         
-        function playRound(humanChoice, computerChoice){
-        
+function playRound(humanChoice, computerChoice){
+   
         // Took nested if else reference from "geeksforgeeks.org" otherwise code would have been multi line 
             
-            if (humanChoice === computerChoice){
-                console.log("It's a tie!");
-                alert("It's a tie!");
-            }
-            else if (humanChoice === "rock"){
-                if (computerChoice === "paper"){
-                    console.log("You lose! Paper beats rock.");
-                    computerScore++;
-                }
-                else {
-                    console.log("You win! Rock beats scissor.");
-                    humanScore++;
-                }
-            }
-            else if (humanChoice === "paper"){
-                if (computerChoice === "scissor"){
-                    console.log("You lose! Scissor beats paper");
-                    computerScore++;
-                }
-                else {
-                    console.log("You win! Paper beats rock.");
-                    humanScore++;
-                }
-            }
-            else if(humanChoice === "scissor"){
-                if (computerChoice === "rock"){
-                    console.log("You lose! Rock beats scissor.");
-                    computerScore++;
-                }
-                else {
-                    console.log("You win! Scissor beats paper.");
-                    humanScore++;
-                }
-            }
+    if (humanChoice === computerChoice){
+        announcementBox.textContent = "It's a tie!";
+    }
+    else if (humanChoice === "rock"){
+        if (computerChoice === "paper"){
+            announcementBox.textContent = "You lose! Paper beats rock.";
+            computerScore++;
+            computerScoreBox.textContent = computerScore;
         }
-        
-        playRound(humanSelection, computerSelection);
-        console.log(`Your score: ${humanScore}`);
-        console.log(`Computer score: ${computerScore}`);
-        
-        
-        }, "100");
-}
+        else {
+            announcementBox.textContent = "You win! Rock beats scissor.";
+            humanScore++;
+            playerScoreBox.textContent = humanScore;
+        }
+    }
+    else if (humanChoice === "paper"){
+        if (computerChoice === "scissor"){
+            announcementBox.textContent = "You lose! Scissor beats paper";
+            computerScore++;
+            computerScoreBox.textContent = computerScore;
+        }
+        else {
+            announcementBox.textContent = "You win! Paper beats rock.";
+            humanScore++;
+            playerScoreBox.textContent = humanScore;
+        }
+    }
+    else if(humanChoice === "scissor"){
+        if (computerChoice === "rock"){
+            announcementBox.textContent = "You lose! Rock beats scissor.";
+            computerScore++;
+            computerScoreBox.textContent = computerScore;
+        }
+        else {
+            announcementBox.textContent = "You win! Scissor beats paper.";
+            humanScore++;
+            playerScoreBox.textContent = humanScore;
+        }
+    }
+    
+    // Winner alert section
 
-setTimeout(()=> {if(humanScore>computerScore){
-    console.log("You won against computer!");
-    text.textContent = "You won against computer!";
+    setTimeout(() => {if(humanScore == 5){
+        alert("YOU WON AGAINST COMPUTER!");
+        location.reload();
+    }
+    else if(computerScore == 5){
+        alert("COMPUTER WON AGAINST YOU!");
+        location.reload();
+    }
+    }, 100);    
 }
-else if (humanScore === computerScore) {
-    console.log("It's draw!");
-    text.textContent = "It's draw!";
-}
-else {
-    console.log("Computer won!");
-    text.textContent = "Computer won!";
-}
-}, "100");
+        
+      
 
-}
+        
+        
+       
+
+
+
+
+
 
 
 
